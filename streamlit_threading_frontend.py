a6ba60a921ba042809df84e5ca6a9939ebf5c088
Victor Janni <54091482+victorjanni@users.noreply.github.com>
2026-08-22 07:29:50 +0530

    side bar added

--- streamlit_threading_frontend.py
+++ streamlit_threading_frontend.py
@@ -19,10 +19,42 @@ def add_thread(thread_id):
     if thread_id not in st.session_state['chat_threads']:
         st.session_state['chat_threads'].append(thread_id)
 
-def load_conversion(thread_id):
+def load_conversation(thread_id):
     state = chatbot.get_state(config={'configurable': {'thread_id': thread_id}})
     # check if messages key exists in state values , return empty list if not 
     return state.values.get('messages', [])
 
 # ***** session setup *****
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
 
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = []
+
+add_thread(st.session_state['thread_id'])
+
+#******** side bar UI********
+
+st.sidebar.tittle('LangGraph-Chatbot')
+
+if st.sidebar.button('new chat'):
+    reset_chat()
+st.sidebar.header('My Conversations')
+
+for thread_id in st.session_state['chat_threads'][::-1]:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = load_conversation(thread_id)
+
+        temp_messages = []
+
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role='user'
+            else:
+                role='assistant'
+            temp_messages.append({'role': role, 'content': msg.content})
+        st.session_state['message_history'] = temp_messages
